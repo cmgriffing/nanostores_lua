82b0bde42dffa997c2ef9d166c25ffa010b43602
Chris Griffing <cmgriffing@gmail.com>
2021-09-08 14:13:09 -0700

    fix: remove src directory

--- src/keep-active/init.lua
+++
@@ -1,9 +0,0 @@
---[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
-local ____exports = {}
-function ____exports.keepActive(store)
-    store:listen(
-        function()
-        end
-    )
-end
-return ____exports
